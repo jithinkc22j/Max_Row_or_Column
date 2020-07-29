083103c33d4c0f3e530bf330fe9a6d4439ca2cbb
JITHIN K C <jithinkc22@gmail.com>
2020-07-29 02:20:00 -1000

    Update main.m

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 % Main fuction - finding row/column max element
 %-------------------------------------------------------------------------
-
+web('https://docs.google.com/document/d/1AbCxFoUhdOCppM8novgCdOv0F9mqYe7HlBU7yX7Svx0/edit?usp=sharing')
+web('published_work.html')
 %-------------------------------------------------------------------------
 
 % DEMO:
